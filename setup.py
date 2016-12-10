688e54d2347024968d829767141b618031355846
Marc-Andre Schulz <marc.schulz@rwth-aachen.de>
2016-12-10 02:27:09 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 setup(
+    name='rnglib',
+    version='0.1.0',
+    author='Marc-Andre Schulz',
+    author_email='marc.schulz@rwth-aachen.de',
+    description='Tools for analyzing human-generated random number sequences',
+    packages=['rnglib'],
     ext_modules=cythonize(Extension('rnglib.dlscore', ["rnglib/dlscore.pyx"]), )
 )
