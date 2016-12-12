e7bb4e9f451a07e36a604272c92621fdcb6d7e88
Marc-Andre Schulz <marc.schulz@rwth-aachen.de>
2016-12-12 22:53:18 +0100

    Add tooly.py (progress bar)

---
+++ rnglib/tools.py
@@ -0,0 +1,38 @@
+import sys
+import time
+
+from numpy import mean, diff
+
+
+def sec_to_str(seconds):
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    return "%d:%02d:%02d" % (h, m, s)
+
+
+def progress(iterator, name=""):
+    length = len(iterator)
+    size = 50
+    timer = []
+
+    def update(iteration):
+        done = size * iteration // length
+
+        timer.append(time.time())
+        if len(timer) > 1:
+            avg_time = mean(diff(timer))
+            remaining_time = sec_to_str(avg_time * (length - iteration))
+        else:
+            remaining_time = "?:?:?"
+
+        sys.stdout.write(
+            "%s[%s%s] %i/%i %s \r" % (name, "#" * done, "." * (size - done), iteration, length, remaining_time))
+        sys.stdout.flush()
+
+    update(0)
+    for i, item in enumerate(iterator):
+        yield item
+        update(i + 1)
+
+    sys.stdout.write("\n")
+    sys.stdout.flush()
