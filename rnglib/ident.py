8144d4c259c814b4a9fb0669aada9478fd2fc4c2
Marc-Andre Schulz <marc.schulz@rwth-aachen.de>
2016-12-15 11:37:49 +0100

    rewrite ident.py

--- rnglib/ident.py
+++ rnglib/ident.py
@@ -9,28 +9,29 @@ def distance(sequence_a, sequence_b, n):
     return norm(diff)
 
 
-def prepare(sequences):
+def compute_scores(sequences, max_n=4, c_sub=1, c_ins=1, c_del=1, c_trans=1, repeats=True):
+    scores = []
+    for s_a, s_b in progress(sequences):
+        f_a = [array_scores(s_a, n, c_sub, c_ins, c_del, c_trans, repeats) for n in range(1, max_n + 1)]
+        f_b = [array_scores(s_b, n, c_sub, c_ins, c_del, c_trans, repeats) for n in range(1, max_n + 1)]
+        scores.append([f_a, f_b])
+    return scores
+
+
+def prepare(scores, feature_func=lambda a, b: [a, b]):
+    def add_case(s_a, s_b, match):
+        raw.append(feature_func(s_a, s_b))
+        classes.append(match)
+
     raw, classes = [], []
-    l = len(sequences)
-    for i, (s_a, s_b) in enumerate(sequences):
-        for j, (s_c, s_d) in enumerate(sequences[:i + 1]):
+    l = len(scores)
+    for i, (s_a, s_b) in enumerate(scores):
+        for j, (s_c, s_d) in enumerate(scores[:i + 1]):
             if i == j:
-                raw.append([s_a, s_b])
-                classes.append(1)
+                add_case(s_a, s_b, 1)
             else:
-                raw.append([s_a, s_c])
-                classes.append(0)
-                raw.append([s_a, s_d])
-                classes.append(0)
-                raw.append([s_b, s_c])
-                classes.append(0)
-                raw.append([s_b, s_d])
-                classes.append(0)
+                add_case(s_a, s_c, 0)
+                add_case(s_a, s_d, 0)
+                add_case(s_b, s_c, 0)
+                add_case(s_b, s_d, 0)
     return raw, classes
-
-
-def compute(raw, diff_func):
-    distances = []
-    for s_a, s_b in progress(raw):
-        distances.append(diff_func(s_a, s_b))
-    return distances
