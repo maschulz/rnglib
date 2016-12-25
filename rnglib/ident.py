a2dcef531753b355c42a77a9a77a774e6c29c1b0
Marc-Andre Schulz <marc.schulz@rwth-aachen.de>
2016-12-25 23:32:56 +0100

    ident doc-strings

--- rnglib/ident.py
+++ rnglib/ident.py
@@ -1,24 +1,53 @@
-from numpy.linalg import norm
+from numpy import mean, std
 from rnglib.dlscore import array_scores
 
 from rnglib.tools import progress
 
 
-def distance(sequence_a, sequence_b, n):
-    diff = array_scores(sequence_a, n) - array_scores(sequence_b, n)
-    return norm(diff)
+def compute_scores(sequences, max_n=4, c_sub=1, c_ins=1, c_del=1, c_trans=1, repeats=True, scale=True):
+    """
+    compute score matrix for list of sequence pairs
 
-
-def compute_scores(sequences, max_n=4, c_sub=1, c_ins=1, c_del=1, c_trans=1, repeats=True):
+    :param sequences: [[s0_0, s0_1],[s1_0,s1_1],...] - list of sequence pairs, one pair per subject
+    :param max_n: int - calculate scores for patterns up to length max_n
+    :param repeats: bool - allow patterns containing repeats?
+    :return: scores(float) in list[subjects][sequence_index][pattern_length-1][pattern_index]
+    """
     scores = []
     for s_a, s_b in progress(sequences):
         f_a = [array_scores(s_a, n, c_sub, c_ins, c_del, c_trans, repeats) for n in range(1, max_n + 1)]
         f_b = [array_scores(s_b, n, c_sub, c_ins, c_del, c_trans, repeats) for n in range(1, max_n + 1)]
         scores.append([f_a, f_b])
+
+    if scale:
+        print('scaling: ', end='')
+        for n in range(max_n):
+            all_scores = [i[n] for i, j in scores] + [j[n] for i, j in scores]
+            mu = mean(all_scores, 0)
+            sd = std(all_scores, 0)
+            for i, j in scores:
+                for k in [i, j]:
+                    k[n] -= mu
+                    k[n] /= sd
+            print('%i. ' % n, end='')
+        print()
     return scores
 
 
 def prepare(scores, feature_func=lambda a, b: [a, b]):
+    """
+    prepare scores for classification
+
+    create cartesian product of sequences with class labels (1 - same subject, 0 - different subjects)
+    process each pair via feature_func(s_a[pattern_length-1][pattern_index], s_b[pattern_length-1][pattern_index])
+    e.g. lambda a, b: norm(a[2]-b[2]) returns length-3 score-distance as sole feature
+    lambda a, b: a[2]-b[2] returns length-3 score-array features
+
+    :param scores: score matrix - from compute_scores()
+    :param feature_func: function(score_array, score_array)
+    :return: list of features, list of classes
+    """
+
     def add_case(s_a, s_b, match):
         raw.append(feature_func(s_a, s_b))
         classes.append(match)
